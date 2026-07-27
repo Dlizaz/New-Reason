8e617e8760e286131dff864109666c07175c0889
Dlizaz <phammthuyyanhh@gmail.com>
2026-07-27 13:10:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
-
-// Import hàm xử lý sự kiện từ file messageCreate.js
 const handleMessageCreate = require('./event/messageCreate');
 
-// Khởi tạo Client
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -13,13 +10,10 @@ const client = new Client({
     ]
 });
 
-// Khi bot đăng nhập thành công
 client.once('ready', () => {
     console.log(`✅ Bot đã sẵn sàng hoạt động dưới tên: ${client.user.tag}`);
 });
 
-// Lắng nghe sự kiện người dùng gửi tin nhắn
 client.on('messageCreate', (message) => handleMessageCreate(client, message));
 
-// Đăng nhập bot
 client.login(process.env.TOKEN);
